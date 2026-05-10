e5379c8a19eeb3f8933ca9f22fd22796afef1de8
ximeblush <ximeblush@hotmail.com>
2026-05-10 20:02:15 -0300

    Agrego clase 3 y 4

--- .vscode/ejerciciosClase/src/Clase_3.java
+++ .vscode/ejerciciosClase/src/Clase_3.java
@@ -1,44 +1,98 @@
 import java.util.ArrayList;
 
 public class Clase_3 {
+
+    //Tené una lista de productos con nombres desprolijos.
+    //Creá un método que recorra la lista y aplique el formateo a cada nombre, volviendo a imprimir la lista con nombres prolijos.
+
+    public static String formatearDescripcion(String descripcion) {
+
+        String[] palabras = descripcion.trim().toLowerCase().split(" ");
+        String resultado = "";
+
+        for (String palabra : palabras) {
+            resultado += palabra.substring(0, 1).toUpperCase() + palabra.substring(1) + " ";
+        }
+        return resultado.trim();
+    }
+
     public static void main(String[] args) throws Exception {
 
-        String[] bebidas = new String[3];
+        // Dada una cadena " té CHAi ", formateala para que quede "Té Chai".
+        // Mostrá su longitud, su primera letra y verificá si contiene la palabra
+        // "Chai".
+
+        String bebida = " té CHAi ";
+        System.out.println("String original: " + bebida);
+        bebida = formatearDescripcion(bebida);
+        System.out.println("String formateado: " + bebida);
+        System.out.println("Longitud: " + bebida.length());
+        System.out.println("Primera letra: " + bebida.substring(0, 1));
+        if (bebida.contains("Chai")) {
+            System.out.println("Sí, tiene Chai");
+        } else {
+            System.out.println("No tiene Chai");
+        }
+        System.out.println("¿Contiene 'Chai'? " + bebida.contains("Chai"));
+
+        // Creá un array de 5 productos.
+        // Imprimí sus elementos y luego intentá agregar un sexto producto (analizá el
+        // resultado).
+
+        String[] bebidas = new String[5];
         bebidas[0] = "Agua SIN gas ";
         bebidas[1] = " Agua con gas ";
         bebidas[2] = " GASEOSA";
-        
-        //bebidas.add(" Milanesa con papas fritas "); //No se permite adherir en un String
-        
-        ArrayList<String> listaProductos = new ArrayList<>();
-        listaProductos.add(" Milanesa con papas fritas ");
-        listaProductos.add("Empanadas");
-        listaProductos.add("Locro");
+        bebidas[3] = "Jugo";
+        bebidas[4] = "Limonada";
 
-        //System.out.println(listaProductos[0]);
+        /*
+         * o se puede escribir asi: ¿?
+         * 
+         * String[] bebidas = {
+         * "Agua SIN gas ",
+         * " Agua con gas ",
+         * " GASEOSA",
+         * "Jugo",
+         * "Limonada"
+         * };
+         * 
+         */
 
-        for (int i = 0 ; i < bebidas.length; i++) {
+        for (int i = 0; i < bebidas.length; i++) {
             System.out.println(bebidas[i]);
             System.out.println(formatearDescripcion(bebidas[i]));
         }
 
-        if (listaProductos.contains("papas")) {
-            System.out.println("Sí, tenemos papas");
-        } else {
-            System.out.println("No tenemos papas");
+        // bebidas.add(" Milanesa con papas fritas "); //No se permite adherir en un
+        // String
+
+        // Creá un ArrayList<String> para productos.
+        // Agregá varios productos, eliminá uno, verificá si otro existe y luego imprimí la lista final.
+        
+        ArrayList<String> listaProductos = new ArrayList<>();
+        listaProductos.add(" Milanesa con papas fritas ");
+        listaProductos.add("Empanadas");
+        listaProductos.add("Locrrrrrro");
+        listaProductos.add("Locro");
+        
+                //System.out.println(listaProductos[0]);
+        for (int i = 0; i < listaProductos.size(); i++) {
+            System.out.println(listaProductos.get(i));
         }
 
-        
-        public static String formatearDescripcion(String descripcion) {
-        
-        String[] palabras = descripcion.trim().toLowerCase().split(" ");
-            String resultado = "";
+        listaProductos.remove("Locrrrrrro");
+        Boolean existe = false;
 
-            for (String palabra : palabras) {
-                resultado += palabra.substring(0, 1).toUpperCase() + palabra.substring(1) + " ";
+        for (int i = 0; i < listaProductos.size(); i++) {
+            System.out.println(listaProductos.get(i));
+            System.out.println(formatearDescripcion(listaProductos.get(i)));
+            if (existe == false) {
+                existe = listaProductos.get(i).contains("papas");
             }
-            return resultado.trim();
         }
+
+        System.out.println("¿Hay papas? " + existe);
 
     }
 }
---
+++ .vscode/ejerciciosClase/src/Clase_4.java
@@ -0,0 +1,81 @@
+public class Clase_4 {
+    //Escribí un método calcularImpuesto que reciba un precio y un porcentaje de impuesto y devuelva el precio final.
+    
+    public static double calcularImpuesto(double precio, double porcentaje) {
+        double impuesto = precio * (porcentaje/100);
+        double precioConImpuesto = precio + impuesto;
+        return precioConImpuesto;
+    }
+
+    //Creá un método calcularPrecioFinal que reciba el precio, el descuento y la cantidad. Si la cantidad es mayor a 50, aumentá el descuento. Imprimí el resultado.
+    
+    public static double calcularPrecioFinal (double precio, double porcentaje, int cantidad) {
+         if (cantidad > 50) {
+            porcentaje += 5;
+        }        
+        double descuento = precio * (porcentaje/100);
+        double precioConDescuento = precio - descuento;
+        return precioConDescuento * cantidad;
+    }
+
+    //Escribí un método reponerStock que sume unidades a un índice específico del array.
+    
+    public static void reponerStock (int[] stock, int indice, int cantidad) {
+        stock[indice] += cantidad;
+    }
+
+    //Creá dos métodos mostrarMensaje: uno recibe una String, otro recibe una String y un int.
+    //Usá ambos para imprimir mensajes en función de si el cliente es nuevo o recurrente.
+
+    public static void mostrarMensaje (String mensaje) {
+        System.out.println(mensaje);
+    }
+
+    public static void mostrarMensaje (String mensaje, int numero) {
+        System.out.println(mensaje + " " + numero);
+    }
+
+    public static void main(String[] args) {
+        Double precio = 100.0;
+        Double impuesto = 21.0;
+        Double descuento = 10.0;
+        int cantidad = 1;
+
+        //Escribí un método calcularImpuesto que reciba un precio y un porcentaje de impuesto y devuelva el precio final.
+        //Llamá al método desde main con distintos valores.
+        System.out.println(calcularImpuesto(precio, impuesto));
+        
+        precio = 1000.0;
+        impuesto = 10.5;
+        System.out.println(calcularImpuesto(precio, impuesto));
+
+        //Creá un método calcularPrecioFinal que reciba el precio, el descuento y la cantidad. Si la cantidad es mayor a 50, aumentá el descuento. Imprimí el resultado.
+        
+        System.out.println(calcularPrecioFinal(precio, descuento, cantidad));
+
+        cantidad = 100;
+        System.out.println(calcularPrecioFinal(precio, descuento, cantidad));
+    
+        //Definí un array con el stock de 3 productos.
+        //Escribí un método reponerStock que sume unidades a un índice específico del array.
+        //Mostrá el stock antes y después de la reposición.
+
+        int[] stock = {1, 30, 100};
+
+        for (int i = 0; i < stock.length; i++) {
+            System.out.println(stock[i]);
+        }
+
+        reponerStock(stock, 0, 10);
+        
+        for (int i : stock) {
+            System.out.println(i);
+        }
+
+        //Usá ambos para imprimir mensajes en función de si el cliente es nuevo o recurrente.
+        mostrarMensaje("Bienvenido! Su número de cliente es: ", 154);
+        mostrarMensaje("Gracias por volver");
+
+    }
+
+}
